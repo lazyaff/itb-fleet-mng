cd57a802643daba4b4aa4882d766ebcca7cf8598
DevOps <devops@komuto.local>
2026-05-09 16:54:10 +0000

    fix: add type annotation to item parameter in gps-tracker

--- app/api/v1/gps-tracker/route.ts
+++ app/api/v1/gps-tracker/route.ts
@@ -93,7 +93,7 @@ export async function GET(request: NextRequest) {
       }),
     ]);
 
-    const data = rawData.map((item, index) => {
+    const data = rawData.map((item: any, index) => {
       return {
         no: offset + index + 1,
         id: item.id,
